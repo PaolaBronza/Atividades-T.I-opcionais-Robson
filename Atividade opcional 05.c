14eb71df5a2adcaa1ab9af788bcbea1a19262fb7
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-03-16 18:50:20 -0300

    Rename Atividade opcional 05.cpp to Atividade opcional 05.c

--- Atividade opcional 05.c
+++ Atividade opcional 05.c
@@ -11,10 +11,10 @@ int main(void){
 	scanf("%c",&let);
 	
 	if(strcmp(let,"A")==0||strcmp(let,"a")==0||strcmp(let,"E")==0||strcmp(let,"e")==0||strcmp(let,"I")==0||strcmp(let,"i")==0||strcmp(let,"O")==0||strcmp(let,"o")==0||strcmp(let,"U")==0||strcmp(let,"u")==0){
-		printf("\na letra inserida é uma vogal");
+		printf("\na letra inserida Ã© uma vogal");
 	}
 	else{
-		printf("\na letra inserida é uma consoante");
+		printf("\na letra inserida Ã© uma consoante");
 	}
 	return 0;
 }
